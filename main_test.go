3e17f62865cdc8be54ae74c1794dee363a6489e0
HAYAMA_Kaoru <iyahaya@nifty.com>
2023-01-21 20:52:57 +0900

    Add test: TestSeekToSignatureForCentralDirectoryHeader

--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package uncozip
 
 import (
+	"io"
 	"strings"
 	"testing"
 )
@@ -11,6 +12,24 @@ func TestSeekToSignatureForLocalHeader(t *testing.T) {
 	err := seekToSignature(r, &output)
 	if err != nil {
 		t.Fatal(err.Error())
+		return
+	}
+	if out := output.String(); out != "HOGEHOGE" {
+		t.Fatalf("output: expect 'HOGEHOGE' but '%s'", out)
+		return
+	}
+}
+
+func TestSeekToSignatureForCentralDirectoryHeader(t *testing.T) {
+	var output strings.Builder
+	r := strings.NewReader("HOGEHOGEPK\x01\x02")
+	err := seekToSignature(r, &output)
+	if err == nil {
+		t.Fatal("expected io.EOF")
+		return
+	}
+	if err != io.EOF {
+		t.Fatalf("expected io.EOF but %s", err.Error())
 		return
 	}
 	if out := output.String(); out != "HOGEHOGE" {
